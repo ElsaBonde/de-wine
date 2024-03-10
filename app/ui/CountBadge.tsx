3dc77e41086d43ceefc7f7a1d527adeaa5a70a88
Elsa Bonde <elsasofiabonde@gmail.com>
2024-03-10 20:03:50 +0100

    fixed so it doesnt show 0 when cart is empty

--- app/ui/CountBadge.tsx
+++ app/ui/CountBadge.tsx
@@ -12,9 +12,19 @@ export default function CountBadge() {
   useEffect(() => {
     // Beräkna totala antalet varor i kundvagnen
     const items = getCartItems();
+    if (items.length === 0) {
+      setTotalQuantity(0);
+      return;
+    } else {
     const total = items.reduce((total, item) => total + item.quantity, 0);
     setTotalQuantity(total);
+    }
   }, [getCartItems]);
+
+  if (totalQuantity === 0) {
+    return null;
+  }
+
 
   return (
     <Typography component="span" data-cy="cart-items-count-badge" sx={{color: "#881C1C"}}> {totalQuantity}
