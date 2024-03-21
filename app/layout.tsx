22a24d16ea17f60b09d07a7b7da51c03542bf1da
malin <malleballe90@gmail.com>
2024-03-21 14:41:11 +0100

    footer styling
    Co-authored-by: Elsa Bonde <ElsaBonde@users.noreply.github.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import "@fontsource/inspiration";
+import "@fontsource/josefin-sans";
 import { AdminPanelSettings, ShoppingCart } from "@mui/icons-material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import XIcon from "@mui/icons-material/X";
 import { Box, Typography } from "@mui/material";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import type { Metadata } from "next";
@@ -92,21 +96,81 @@ export default function RootLayout({ children }: LayoutProps) {
                       background: "#f1ddcf",
                       marginTop: "auto",
                       padding: "5px 10px",
-                      alignItems: "center",
-                      justifyContent: "space-between",
+                      fontFamily: "Josefin sans",
+                      color: "#5A5353",
                     }}
                   >
                     <Link
                       href="/admin"
                       style={{
                         textDecoration: "none",
-                        color: "#881C1C",
+                        color: "#5A5353",
+                        display: "flex",
+                        alignItems: "center",
                       }}
                       data-cy="admin-link"
                     >
-                      <AdminPanelSettings /> Admin
+                      <AdminPanelSettings
+                        sx={{ "&:hover": { color: "#881c1c" } }}
+                      />
+                      Admin
                     </Link>
-                    <Typography>© 2024</Typography>
+
+                    <Box
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        textAlign: "center",
+                        marginX: "auto",
+                        /*  gap: "3px", */
+                      }}
+                    >
+                      <Box
+                        sx={{
+                          display: "flex",
+                          flexDirection: "row",
+                          justifyContent: "center",
+                        }}
+                      >
+                        <InstagramIcon
+                          sx={{
+                            color: "#881c1c",
+                            width: "30px",
+                            height: "30px",
+                          }}
+                        />
+                        <FacebookIcon
+                          sx={{
+                            color: "#881c1c",
+                            width: "30px",
+                            height: "30px",
+                          }}
+                        />
+                        <XIcon
+                          sx={{
+                            color: "#881c1c",
+                            width: "30px",
+                            height: "30px",
+                          }}
+                        />
+                      </Box>
+                      <Link
+                        style={{
+                          textDecoration: "none",
+                          color: "#5A5353",
+                          lineHeight: "1.5",
+                        }}
+                        href="mailto:costumerservice@wineoclock.com"
+                      >
+                        costumerService@wineoclock.com
+                      </Link>
+                      <Typography sx={{ fontFamily: "Josefin sans" }}>
+                        +46 31-487 30 73
+                      </Typography>
+                      <Typography sx={{ fontFamily: "Josefin sans" }}>
+                        © 2024 Wine O`clock
+                      </Typography>
+                    </Box>
                   </Box>
                 </SnackbarProvider>
               </AppRouterCacheProvider>
