dd34d2201f56142d3a9064e339aae980d91ab8f0
malin <malleballe90@gmail.com>
2024-03-21 13:09:15 +0100

    productpage styled
    Co-authored-by: Elsa Bonde <ElsaBonde@users.noreply.github.com>

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,7 +1,8 @@
 import AddButton from "@/app/ui/AddButton";
-import { getProductById } from "@/data";
+import { getProductById, products } from "@/data";
 import "@fontsource/josefin-sans";
-import { Box, CardMedia, Typography } from "@mui/material";
+import { Box, CardMedia, Divider, Link, Typography } from "@mui/material";
+import NextLink from "next/link";
 
 type PageProps = { params: { slug: string } };
 
@@ -22,75 +23,177 @@ export default function ProductPage({ params }: PageProps) {
         padding: "10px 20px",
         background: "#F9F1EC",
         display: "flex",
-        flexDirection: { xs: "column", md: "row" },
+        flexDirection: "column",
+        gap: "25px",
       }}
     >
-      <Box component="div" sx={{ width: "100%", height: "40vh" }}>
-        <CardMedia
-          component="img"
-          image={product.image}
-          alt={product.title}
-          sx={{
-            maxWidth: "100%",
-            height: "auto",
-          }}
-        />
-      </Box>
       <Box
         component="div"
         sx={{
-          backgroundColor: "white",
-          borderRadius: "15px",
-          padding: "20px 40px",
+          display: "flex",
+          flexDirection: {
+            xs: "column",
+            sm: "column",
+            md: "row",
+          },
         }}
       >
-        <Box
+        <Box component="div" sx={{ width: "100%", height: "40vh" }}>
+          <CardMedia
+            component="img"
+            image={product.image}
+            alt={product.title}
+            sx={{
+              maxWidth: "100%",
+              height: "auto",
+              minHeight: "40vh",
+              borderRadius: {
+                xs: "15px 15px 0px 0px",
+                md: "15px 0px 0px 15px",
+              },
+            }}
+          />
+        </Box>
+
+        <Box /* boxjäveln där informationen ligger */
           component="div"
-          sx={{ display: "flex", justifyContent: "space-between" }}
+          sx={{
+            backgroundColor: "white",
+            borderRadius: { xs: "0px 0px 15px 15px", md: "0px 15px 15px 0px" },
+            padding: "20px 40px",
+          }}
         >
-          <Box component="div">
+          <Box
+            component="div"
+            sx={{ display: "flex", justifyContent: "space-between" }}
+          >
+            <Box component="div">
+              {" "}
+              {/* boxen där texten och sån ligger */}
+              <Typography
+                variant="h5"
+                data-cy="product-title"
+                sx={{ fontFamily: "josefin sans" }}
+              >
+                {product.title}
+              </Typography>
+              <Box sx={{ display: "flex", gap: "15px" }}>
+                <Typography
+                  data-cy="product-price"
+                  sx={{
+                    fontFamily: "josefin sans",
+                    color: "black",
+                    marginBottom: "10px",
+                    ...(product.salePrice && {
+                      textDecoration: "line-through",
+                    }),
+                  }}
+                >
+                  {product.price} :-
+                </Typography>
+
+                {product.salePrice && (
+                  <Typography
+                    sx={{
+                      fontFamily: "josefin sans",
+                      color: "red",
+                      marginBottom: "10px",
+                    }}
+                  >
+                    {product.salePrice} :-
+                  </Typography>
+                )}
+              </Box>
+            </Box>
+            <AddButton product={product} />
+          </Box>
+          <Typography
+            data-cy="product-description"
+            sx={{ marginTop: "15px", fontFamily: "josefin sans" }}
+          >
+            {product.description}
+          </Typography>
+          <Divider sx={{ margin: "20px 0px" }} />
+          <Box>
             <Typography
               variant="h5"
-              data-cy="product-title"
-              sx={{ fontFamily: "josefin sans" }}
+              sx={{ fontFamily: "josefin sans", marginBottom: "10px" }}
             >
-              {product.title}
+              Fits well with:
             </Typography>
-
-            <Box sx={{ display: "flex", gap: "15px" }}>
-              <Typography
-                data-cy="product-price"
+            <Typography sx={{ fontFamily: "josefin sans" }}>
+              {product.compatibility}
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+      <Divider />
+      <Box
+        sx={{
+          display: { xs: "none", md: "block" },
+        }}
+      >
+        <Typography
+          variant="h6"
+          sx={{
+            textAlign: "center",
+            fontFamily: "josefin sans",
+            marginBottom: "15px",
+          }}
+        >
+          YOU MAY ALSO LIKE:
+        </Typography>
+        <Box
+          sx={{
+            display: { xs: "none", md: "flex" },
+            flexDirection: "row",
+            gap: "15px",
+          }}
+        >
+          {products.slice(0, 3).map((product) => (
+            <Link
+              component={NextLink}
+              href={`/product/${product.id}`}
+              key={product.id}
+              sx={{
+                textDecoration: "none",
+                color: "black",
+              }}
+            >
+              <Box
+                key={product.id}
                 sx={{
-                  fontFamily: "josefin sans",
-                  color: "black",
-                  marginBottom: "10px",
-                  ...(product.salePrice && { textDecoration: "line-through" }),
+                  marginBottom: "15px",
+                  backgroundColor: "rgba(255, 255, 255, 0.5)",
+                  borderRadius: "10px",
+                  "&:hover": {
+                    transform: "scale(1.02)",
+                    backgroundColor: "white",
+                    transition: "transform 0.6s",
+                  },
                 }}
               >
-                {product.price} :-
-              </Typography>
-
-              {product.salePrice && (
+                <CardMedia
+                  component="img"
+                  image={product.image}
+                  alt={product.title}
+                  sx={{ borderRadius: "10px 10px 0px 0px" }}
+                ></CardMedia>
                 <Typography
                   sx={{
                     fontFamily: "josefin sans",
-                    color: "red",
-                    marginBottom: "10px",
+                    marginTop: "5px",
+                    padding: "15px",
+                    textDecoration: "underline",
+                    textUnderlineOffset: "3px",
                   }}
                 >
-                  {product.salePrice} :-
+                  {product.title}
                 </Typography>
-              )}
-            </Box>
-          </Box>
-          <AddButton product={product} />
+              </Box>
+            </Link>
+          ))}
         </Box>
-        <Typography
-          data-cy="product-description"
-          sx={{ marginTop: "15px", fontFamily: "josefin sans" }}
-        >
-          {product.description}
-        </Typography>
       </Box>
     </Box>
   );
