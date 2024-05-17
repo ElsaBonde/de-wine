506b5177686cd40c47c6271b9aae6a603acc0475
Elsa Bonde <elsasofiabonde@gmail.com>
2024-05-17 10:31:28 +0200

    moved out validation for login and register
    Co-authored-by: Elin <mypinkworld@users.noreply.github.com>
    Co-authored-by: Navas <Navaas@users.noreply.github.com>

--- app/ui/LoginForm.tsx
+++ app/ui/LoginForm.tsx
@@ -10,19 +10,8 @@ import {
 } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
-import { z } from "zod";
-
-export const LoginSchema = z.object({
-  email: z.string().email({
-    message:
-      "Please enter a valid email address in the following format: name@example.com",
-  }),
-
-  password: z.string().min(1, { message: "Wrong email or password" }),
-});
-
-//typ för meddelandet
-export type Customer = z.infer<typeof LoginSchema>;
+import { LoginSchema } from "../validation/login";
+import { Customer } from "./CustomerContext";
 
 function LoginForm() {
   const router = useRouter();
