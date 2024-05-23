3495c02c432604ad2f490390000691fb12471157
annaa <annaalehed@hotmail.com>
2024-05-23 19:35:26 +0200

    styling av log in sida och knappar

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,27 +1,90 @@
 import { providerMap, signIn } from "@/auth";
-import { Box } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 
 export default function SignInPage() {
   return (
-    <Box
-      component="div"
-      sx={{ display: "flex", flexDirection: "column", gap: 2 }}
-    >
-      {Object.values(providerMap).map((provider) => (
+    <>
+      <Box component="div" sx={{ display: "flex", height: "100%" }}>
         <Box
-          component="form"
-          key={provider.id}
-          action={async () => {
-            "use server";
-            await signIn(provider.id);
+          component="div"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            flex: "1",
+            alignItems: "center",
+            justifyContent: "center",
           }}
-          sx={{ display: "flex", flexDirection: "column", gap: 2 }}
         >
-          <button type="submit">
-            <span>Sign in with {provider.name}</span>
-          </button>
+          <Typography
+            variant="h4"
+            sx={{
+              color: "#5A5353",
+              fontSize: {
+                xs: "1.2rem",
+                sm: "1.3rem",
+                md: "1.5rem",
+                lg: "2rem",
+              },
+            }}
+          >
+            Please sign in to continue
+          </Typography>
+          {Object.values(providerMap).map((provider) => (
+            <>
+              <Box
+                component="form"
+                key={provider.id}
+                action={async () => {
+                  "use server";
+                  await signIn(provider.id);
+                }}
+              >
+                <Button
+                  variant="contained"
+                  type="submit"
+                  sx={{
+                    minWidth: "200px",
+                    backgroundColor: "#881C1C",
+                    "&:hover": {
+                      backgroundColor: "#440000",
+                    },
+                  }}
+                >
+                  <span>Sign in with {provider.name}</span>
+                </Button>
+              </Box>
+            </>
+          ))}
         </Box>
-      ))}
-    </Box>
+        <Box
+          sx={{
+            flex: 1,
+            backgroundImage: "url('/winebackground.webp')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            color: "#F9F1EC",
+            p: 4,
+            textAlign: "center",
+          }}
+        >
+          {" "}
+          <Typography
+            variant="h2"
+            sx={{
+              fontWeight: "bold",
+              mb: 2,
+              textShadow:
+                "0 0 10px rgba(0, 0, 0, 0.5), 0 0 20px rgba(9, 9, 9, 0.41)",
+            }}
+          >
+            Welcome to DiWine
+          </Typography>
+        </Box>
+      </Box>
+    </>
   );
 }
--- app/ui/SignInButton.tsx
+++ app/ui/SignInButton.tsx
@@ -6,6 +6,14 @@ import { signIn } from "next-auth/react";
 export default function SignInButton() {
   return (
     <Button
+      variant="contained"
+      sx={{
+        minWidth: "100px",
+        backgroundColor: "#881C1C",
+        "&:hover": {
+          backgroundColor: "#440000",
+        },
+      }}
       onClick={() => signIn()}
       className="bg-blue-400 text-white rounded-md p-2"
     >
