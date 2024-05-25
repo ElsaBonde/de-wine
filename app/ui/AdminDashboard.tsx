0f94944aca4d9e60448eca3865eae35ffca0da5e
Elsa Bonde <elsasofiabonde@gmail.com>
2024-05-25 11:06:28 +0200

    admin dashboard functions is done

--- app/ui/AdminDashboard.tsx
+++ app/ui/AdminDashboard.tsx
@@ -3,18 +3,49 @@ import PeopleIcon from "@mui/icons-material/People";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { Box, Grid, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
+import {
+  orderNumber,
+  totalAmountAllOrders,
+} from "../actions/orderActions";
 import { userNumber } from "../actions/userActions";
 
 export default function AdminDashboard() {
   const [usersCount, setUsersCount] = useState<number>(0);
+  const [ordersCount, setOrdersCount] = useState<number>(0);
+  const [totalAmount, setTotalAmount] = useState<number>(0);
 
+
+  //david kan man göra detta på ett bättre sätt eller duger det? 
   useEffect(() => {
     const fetchAndSetUserNumber = async () => {
-      const count = await userNumber();
-      setUsersCount(count);
+      const userCount = await userNumber();
+      setUsersCount(userCount);
     };
 
     fetchAndSetUserNumber();
+  }, []);
+
+  useEffect(() => {
+    const fetchAndSetOrderNumber = async () => {
+      const orderCount = await orderNumber();
+      setOrdersCount(orderCount);
+    };
+
+    fetchAndSetOrderNumber();
+  }, []);
+
+  useEffect(() => {
+    const fetchAndSetTotalAmount = async () => {
+      const total = await totalAmountAllOrders();
+
+      if (total !== null) {
+        setTotalAmount(Math.round(Number(total) * 100) / 100);
+      } else {
+        setTotalAmount(0);
+      }
+    };
+
+    fetchAndSetTotalAmount();
   }, []);
 
   return (
@@ -91,7 +122,7 @@ export default function AdminDashboard() {
                 variant="h5"
                 sx={{ color: "#881c1c", fontFamily: "josefin sans" }}
               >
-                produkter
+                {ordersCount}
               </Typography>
               <Typography
                 variant="h6"
@@ -125,7 +156,7 @@ export default function AdminDashboard() {
                 variant="h5"
                 sx={{ color: "#881c1c", fontFamily: "josefin sans" }}
               >
-                8733279523907
+                {totalAmount}
               </Typography>
               <Typography
                 variant="h6"
