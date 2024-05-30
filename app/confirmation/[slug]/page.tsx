234885fb3cb608c9c912ddbcfb3736f2732e2741
mypinkworld <myworkplace.art@gmail.com>
2024-05-30 15:50:14 +0200

    inproggress

--- app/confirmation/[slug]/page.tsx
+++ app/confirmation/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { getOrderById } from "@/app/actions/orderActions";
 import ClearCart from "@/app/ui/ClearCart";
 import { auth } from "@/auth";
-import { Box, Card, Divider, Typography } from "@mui/material";
+import { Box, Card, Divider, Typography, Button } from "@mui/material";
 import Image from "next/image";
 
 type PageProps = { params: { slug: string } };
@@ -23,170 +23,163 @@ export default async function ConfirmationPage({ params }: PageProps) {
   return (
     <Box
       component="main"
-      sx={{ display: "flex", flexDirection: { xs: "column" } , maxWidth: "800px",  margin: "auto"
-
-    }}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        background: "white",
+        padding: "10px 20px",
+        flex: 1,
+        marginTop: "40px",
+        // maxWidth: "1000px", // Increase maxWidth to make it wider
+        margin: "auto",
+      }}
     >
-        {/*    sektion Thank for your Purschase */}
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            flexGrow: "5",
-            background: "rgba(242, 239, 239, 0.8) ",
-            margin: "50px",
-            borderRadius: "10px",
-          }}
-        >
-          <Typography
-            variant="h4"
-            sx={{
-              textAlign: "center",
-              margin: "10px",
-              color: "black",
-              fontVariant: "small-caps",
-            }}
-          >
-            Thank you for your purchase!
-          </Typography>
-          <Box
-            sx={{ display: "flex", flexDirection: { xs: "column", md: "row" } }}
-          >
-            {order && (
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  flexGrow: "8",
-                  margin: "auto",
-                  textAlign: "left",
-                  padding: "10px 20px",
-                  gap: "3px",
-                }}
-              >
-                <Typography
-                  variant="h5"
-                  sx={{ fontFamily: "karla", marginTop: "20px" }}
-                >
-                  Your Shipping Information:
-                </Typography>
-                <Typography sx={{ fontWeight: "bold" }}>
-                  Your orderId is:
-                </Typography>
-                <Typography component="span" sx={{}}>
-                  {" "}
-                  {order.id}
-                </Typography>
-                <Typography sx={{ fontWeight: "bold" }}>
-                  Name: <Typography component="span">{order.name}</Typography>
-                </Typography>
-                <Typography sx={{ fontWeight: "bold" }}>
-                  Email:{" "}
-                  <Typography component="span">{session?.user?.email}</Typography>
-                </Typography>
-                <Typography sx={{ fontWeight: "bold" }}>
-                  Phone number:{" "}
-                  <Typography component="span">{order.phone}</Typography>
-                </Typography>
-                <Typography sx={{ fontWeight: "bold" }}>
-                  Adress: <Typography component="span">{order.street}</Typography>
-                </Typography>
-                <Typography sx={{ fontWeight: "bold" }}>
-                  Zip code: <Typography component="span">{order.zip}</Typography>
-                </Typography>
-                <Typography sx={{ fontWeight: "bold" }}>
-                  City: <Typography component="span">{order.city}</Typography>
-                </Typography>
-              </Box>
-            )}
-            <Box
-              sx={{
-                display: "flex",
-                flexGrow: "2",
-                justifyContent: "center",
-                alignItems: "center",
-                marginRight: "20px",
-              }}
-            >
-            </Box>
-          </Box>
-        </Box>
+      {/* Thank you for your purchase section */}
       <Box
-        component="div"
         sx={{
-          flexGrow: "4",
           display: "flex",
           flexDirection: "column",
-          backgroundColor: "rgba(242, 239, 239, 0.8)",
-          margin: "10px",
+          flexGrow: "5",
+          background: "rgba(242, 239, 239, 0.8)",
+          margin: "50px",
           borderRadius: "10px",
           padding: "10px",
-          justifyContent: "space-evenly",
         }}
       >
         <Typography
+          variant="h4"
           sx={{
-            color: "black",
-            fontSize: "30px",
             textAlign: "center",
+            margin: "10px",
+            color: "black",
             fontVariant: "small-caps",
-            paddingBottom: "10px",
+            fontFamily: "Karla",
           }}
         >
-          Your Wine&apos;Order:
+          Thank you for your purchase!
         </Typography>
-        <Box>
-          {rows.map((item, index) => (
-            <Card
-              key={item.product.id}
+        <Box sx={{ display: "flex", flexDirection: { xs: "column", md: "row" } }}>
+          {order && (
+            <Box
               sx={{
                 display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-                background: "white",
-                marginBottom: "10px",
-                borderRadius: "0px 10px 10px 0px",
-                padding: "10px",
+                flexDirection: "column",
+                flexGrow: "8",
+                margin: "auto",
+                textAlign: "left",
+                padding: "10px 20px",
+                gap: "3px",
               }}
             >
-              <Image
-                src={item.product.image}
-                alt={item.product.title}
-                height={75}
-                width={75}
-                style={{ height: "100%", width: "auto" }}
-              />
-              <Box
-                sx={{
-                  paddingLeft: "10px",
-                  display: "flex",
-                  flexDirection: "column",
-                  flexGrow: "1",
-                }}
+              <Typography
+                variant="h5"
+                sx={{ fontFamily: "Karla", marginTop: "20px" }}
               >
-                <Typography sx={{ fontFamily: "Josefin Sans" }}>
-                  {item.product.title}
-                </Typography>
+                Your Shipping Information:
+              </Typography>
+              <Typography sx={{ fontWeight: "bold" }}>Your orderId is:</Typography>
+              <Typography component="span">{order.id}</Typography>
+              <Typography sx={{ fontWeight: "bold" }}>
+                Name: <Typography component="span">{order.name}</Typography>
+              </Typography>
+              <Typography sx={{ fontWeight: "bold" }}>
+                Email: <Typography component="span">{session?.user?.email}</Typography>
+              </Typography>
+              <Typography sx={{ fontWeight: "bold" }}>
+                Phone number: <Typography component="span">{order.phone}</Typography>
+              </Typography>
+              <Typography sx={{ fontWeight: "bold" }}>
+                Address: <Typography component="span">{order.street}</Typography>
+              </Typography>
+              <Typography sx={{ fontWeight: "bold" }}>
+                Zip code: <Typography component="span">{order.zip}</Typography>
+              </Typography>
+              <Typography sx={{ fontWeight: "bold" }}>
+                City: <Typography component="span">{order.city}</Typography>
+              </Typography>
+            </Box>
+          )}
+        </Box>
+      </Box>
+
+      {/* Order information  */}
+      <Typography
+        variant="h5"
+        gutterBottom
+        justifyContent={"center"}
+        sx={{
+          fontFamily: "Karla",
+          fontWeight: "800",
+          fontVariant: "small-caps",
+          // marginX: "0", // Set marginX for full width
+          // padding: "0 10px", // Add padding fspace around the text
+          textAlign: "center", 
+        }}
+      >
+        Your Wine Order
+      </Typography>
+      <Box>
+        <Card
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            // padding: "10px",
+            alignItems: "center",
+            background: "white",
+            marginBottom: "40px",
+            padding: "20px",
+            // marginX: "auto", // Center the card page
+            width: "100%", // Make card full width
+            // maxWidth: "1000px", // Control max width to make it bigger???
+          }}
+        >
+          {rows.map((item, index) => (
+            <Box key={item.product.id} sx={{ width: "100%" }}>
+              <Box sx={{ display: "flex" }}>
+                <Box sx={{ marginRight: "10px" }}>
+                  <Image
+                    src={item.product.image}
+                    alt={item.product.title}
+                    height={100}
+                    width={100}
+                    style={{ height: "100%", width: "auto" }}
+                  />
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    flexGrow: 1,
+                    paddingLeft: "10px",
+                  }}
+                >
+                  <Typography sx={{ fontFamily: "Josefin Sans" }}>
+                    {item.product.title}
+                  </Typography>
                   <Typography sx={{ fontFamily: "Josefin Sans" }}>
                     Price: {item.product.price * item.quantity} $
                   </Typography>
-                <Typography sx={{ fontFamily: "Josefin Sans" }}>
-                  {item.quantity} pc
-                </Typography>
+                  <Typography sx={{ fontFamily: "Josefin Sans" }}>
+                    {item.quantity} pc
+                  </Typography>
+                </Box>
               </Box>
-            </Card>
+              <Divider sx={{ margin: "15px 0", background: "#796e6e" }} />
+            </Box>
           ))}
-        </Box>
-        <Divider sx={{ color: "white", padding: "5px" }} />
-        <Typography
-          sx={{
-            fontFamily: "Josefin Sans",
-            textAlign: "center",
-            fontWeight: "bold",
-          }}
-        >
-          TOTAL: {order.total} $
-        </Typography>
+          <Box
+            sx={{
+              color: "black",
+              padding: "5px",
+              fontFamily: "Josefin Sans",
+              textAlign: "center",
+              fontWeight: "bold",
+              width: "100%",
+            }}
+          >
+            TOTAL: {order.total} $
+          </Box>
+        </Card>
       </Box>
 
       <ClearCart />
