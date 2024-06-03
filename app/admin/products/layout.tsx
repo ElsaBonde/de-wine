e63b439491f245b0cb9d59b71cbe6b0325a76a90
mypinkworld <myworkplace.art@gmail.com>
2024-06-03 15:05:26 +0200

    Productlist to use UseState, open close the admin side for each product, fixed ProductRow and took out the Use

--- app/admin/products/layout.tsx
+++ app/admin/products/layout.tsx
@@ -2,6 +2,13 @@ import { getProducts } from "@/app/actions/productActions";
 import { Divider, Grid } from "@mui/material";
 import { PropsWithChildren } from "react";
 import ProductRow from "../ProductRow";
+import ProductList from "../ProductList";
+import { Product } from "@prisma/client";
+
+interface ProductListProps {
+  products: Product[]; 
+}
+
 
 export default async function ProductsPage(props: PropsWithChildren) {
   const products = await getProducts();
@@ -15,8 +22,8 @@ export default async function ProductsPage(props: PropsWithChildren) {
       <Divider sx={{marginTop: "35px"}} />
       <Grid container spacing={4} sx={{ marginTop: "0px" }}>
         {products.map((product) => (
-          <ProductRow key={product.id} product={product} />
-        ))}
+        <ProductList key={products[0].id} products={products} />
+      ))}
       </Grid>
       {props.children}
     </>
