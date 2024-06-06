3503830d10591e53bd0e15cee45868e4b96a7554
annaa <annaalehed@hotmail.com>
2024-06-06 11:53:13 +0200

    you cannot add product if inventory to low
    Co-authored-by: Navas <Navaas@users.noreply.github.com>
    Co-authored-by: Elin <mypinkworld@users.noreply.github.com>

--- app/ui/AddButton.tsx
+++ app/ui/AddButton.tsx
@@ -20,18 +20,22 @@ export default function AddButton(props: Props) {
   //ökar antalet items i kundkorgen, lägger till dem i LS och visar snackbaren
   const handleClick = (e: React.MouseEvent) => {
     e.preventDefault();
-    const productsInCart = cart.filter(
-      (product) => product.id === props.product.id
-    );
 
-    if (props.product.inventory < 1) {
+    const totalQuantityInCart = cart
+      .filter((product) => product.id === props.product.id)
+      .reduce((total, product) => total + product.quantity, 0);
+
+    // Check if adding another item exceeds the inventory
+    if (
+      totalQuantityInCart >= props.product.inventory ||
+      props.product.inventory < 1
+    ) {
       showSnack(props.product.title + " is out of stock.");
     } else {
       showSnack(props.product.title + " has been added to your cart.");
       addToCart(props.product);
     }
   };
-
   return (
     <>
       {props.isProductPage ? (
